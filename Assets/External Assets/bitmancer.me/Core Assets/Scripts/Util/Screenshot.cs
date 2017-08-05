5c6ac8d204a9e15810dfb6e4d908160befe9cc54
Mike Post <mike@bitmancer.me>
2017-08-05 00:25:31 -0700

    Updates to screenshot folder location

--- Assets/External Assets/bitmancer.me/Core Assets/Scripts/Util/Screenshot.cs
+++ Assets/External Assets/bitmancer.me/Core Assets/Scripts/Util/Screenshot.cs
@@ -21,22 +21,43 @@ namespace Bitmancer.Core.Util {
         /// <summary>
         /// Saves a screenshot.
         /// </summary>
-        public static void saveScreenshot() {
-            
+        /// <param name="prefix">Prefix (e.g. the name of the Application or similar).</param>
+        /// <remarks>
+        /// Screenshots are saved as PNGs.
+        /// Screenshots made in the editor will be saved to the desktop, with the <c>prefix</c> used as a filename prefix.
+        /// Screenshots in standalone desktop builds will be saved to the "My Pictures" folder, with the <c>prefix</c> used as a subfolder name.
+        /// Screenshots on mobile will be stored in the application persistent data path, under a <c>kScreenshotDirectory</c> folder (<c>prefix</c> is ignored in this case).
+        /// </remarks>
+        public static void saveScreenshot( string prefix ) {
+
+            var timestamp = DateTime.Now;
+            string filename = string.Format( "{0} at {1}.png", timestamp.ToString( "yyyy-MM-dd" ), timestamp.ToString( "hh.mm.ss.fff" ) );
+
             string outputPath;
 
             if ( Application.isMobilePlatform ) {
                 // roots at persistent path on mobile
                 outputPath = kScreenshotDirectory;
+
             } else {
+                
+#if UNITY_EDITOR_OSX || UNITY_EDITOR_WIN
+                outputPath = System.Environment.GetFolderPath( System.Environment.SpecialFolder.Desktop );
+
+                // adjust the filename since we're saving to the desktop
+                filename = string.Format( "{0} Screenshot {1}", prefix, filename );
+
+#elif UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN
+                outputPath = System.IO.Path.Combine( System.Environment.GetFolderPath( System.Environment.SpecialFolder.MyPictures ), prefix );
+#else
                 outputPath = System.IO.Path.Combine( Application.persistentDataPath, kScreenshotDirectory );
+#endif
             }
 
-            System.IO.Directory.CreateDirectory( outputPath );
+            if ( ! System.IO.Directory.Exists( outputPath ) ) {
+                System.IO.Directory.CreateDirectory( outputPath );
+            }
 
-            var timestamp = DateTime.Now;
-            // Build the timestamp piece-by-piece (my feeling is that this is less error-prone during refactoring/changes than trying to do it all via DateTime.ToString)
-            string filename = string.Format( "Screenshot {0} at {1}.png", timestamp.ToString( "yyyy-MM-dd" ), timestamp.ToString( "hh.mm.ss.fff" ) );
             string screenshotFile = System.IO.Path.Combine( outputPath, filename );
 
             Application.CaptureScreenshot( screenshotFile );
